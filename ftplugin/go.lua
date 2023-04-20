de518617cd6365a3c81a6b51076db5f73b0e46ff
Daniel Bowman <daniel@danielbowman.co.uk>
2023-04-20 13:40:06 +0100

    Add sql snippet

--- ftplugin/go.lua
+++ ftplugin/go.lua
@@ -1,11 +1,11 @@
-vim.api.nvim_create_autocmd("BufWritePost", {
-	pattern = "*.go",
-	callback = function(_)
-		vim.cmd([[
-  let file = expand('%')
-  silent execute "!gofmt -w " . file
-  edit!
-  ]])
-	end,
-	desc = "Format go code on save",
-})
+--vim.api.nvim_create_autocmd("BufWritePost", {
+	--pattern = "*.go",
+	--callback = function(_)
+		--vim.cmd([[
+  --let file = expand('%')
+  --silent execute "!gofmt -w " . file
+  --edit!
+  --]])
+	--end,
+	--desc = "Format go code on save",
+--})
--- init.lua
+++ init.lua
@@ -174,12 +174,12 @@ cmp.setup.cmdline(":", {
 })
 
 -- Setup lspconfig.
-local capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
 -- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
 -- require('lspconfig')['pyright'].setup {
-  --   capabilities = capabilities
-  -- }
-local servers = {'ansiblels', 'vimls', 'bashls', 'terraformls'}
+--   capabilities = capabilities
+-- }
+local servers = { "ansiblels", "vimls", "bashls", "terraformls", "gopls" }
 for _, lsp in ipairs(servers) do
 	nvim_lsp[lsp].setup({
 		on_attach = on_attach,
@@ -214,18 +214,16 @@ vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagn
 	update_in_insert = false,
 })
 
-
-
-vim.filetype.add {
-        pattern = {
-                ['.*'] = {
-                        priority = -math.huge,
-                        function(path, bufnr)
-                                local content = vim.filetype.getlines(bufnr, 1)
-                                if vim.filetype.matchregex(content, [[^#!/usr/bin/env bash]]) then
-                                        return 'bash'
-                                end
-                        end,
-                },
-        },
-}
+vim.filetype.add({
+	pattern = {
+		[".*"] = {
+			priority = -math.huge,
+			function(path, bufnr)
+				local content = vim.filetype.getlines(bufnr, 1)
+				if vim.filetype.matchregex(content, [[^#!/usr/bin/env bash]]) then
+					return "bash"
+				end
+			end,
+		},
+	},
+})
